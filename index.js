bfc1c3c01c98916f9e6acadcd96340ffb4152704
deboker <117391017+deboker@users.noreply.github.com>
2024-04-11 18:33:16 +0200

    design changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
+    const hamburger = document.querySelector('.hamburger'); 
     if (menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
